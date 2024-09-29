ff6918f5c14f95dec1182f1ad8af0b69ed728e59
Shravan Annamalai Sithambaram <136394302+VirtualSquares@users.noreply.github.com>
2024-09-29 10:50:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -90,5 +90,5 @@ def fact_check():
         return jsonify({'error': f"An error occurred: {e}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
 
